b05f85f79b0eb3c611355eab72fc14343bf0b697
Shah <41285362+shahzerbaig@users.noreply.github.com>
2026-07-07 02:58:27 +0530

    feat: add get started and resources pages

---
+++ src/app/get-started/page.tsx
@@ -0,0 +1,8 @@
+export default function GetStartedPage() {
+  return (
+    <div>
+      <h1>Get Started</h1>
+      <p>Welcome to our get started page!</p>
+    </div>
+  );
+}
---
+++ src/app/resources/page.tsx
@@ -0,0 +1,8 @@
+export default function ResourcePage() {
+  return (
+    <div>
+      <h1>Resources</h1>
+      <p>Welcome to our resources page!</p>
+    </div>
+  );
+}
